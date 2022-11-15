db56c672e5b3fcc3e5f2f066f45c1cca7f91dee1
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-15 14:56:59 -0500

    Lab start

---
+++ express-fruits/models/vegetables.js
@@ -0,0 +1,28 @@
+const vegetables = [
+    {
+        name: `cucumber`,
+        color: `green`,
+        readyToEat: true
+    },
+    {
+        name: `avocado`,
+        color: `black`,
+        readyToEat: true
+    },
+    {
+        name: `eggplant`,
+        color: `purple`,
+        readyToEat: true
+    },
+    {
+        name: `tomato`,
+        color: `yellow`,
+        readyToEat: false
+    },
+    {
+        name: `carrots`,
+        color: `orange`,
+        readyToEat: true
+    },
+]
+module.exports = vegetables
---
+++ express-fruits/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+const fruits = require(`./models/fruits.js`)
+const reactViews = require(`express-react-views`)
+
+app.set(`view engine`, `jsx`);
+app.engine(`jsx`, reactViews.createEngine());
+
+app.get("/fruits", (req, res) => {
+  res.render(`Index`, {fruits: fruits})
+})
+
+app.get("/fruits/:indexOfFruit", (req, res) => {
+//   res.send(fruits[req.params.indexOfFruit])
+    res.render(`Show`, {
+        fruit : fruits[req.params.indexOfFruit]
+    })
+})
+
+app.listen(PORT, () => { 
+  console.log(`Listening on port: ${PORT}`)
+}) 
